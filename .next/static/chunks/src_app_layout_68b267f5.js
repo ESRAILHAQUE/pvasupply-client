451cb113800984fca3882a818e9cf0d3a59f6244
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-08-06 15:41:26 +0600

    Remove .next folder from git tracking

--- .next/static/chunks/src_app_layout_68b267f5.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__a2f1cdd3._.css",
-  "static/chunks/_9e03e1a4._.js"
-],
-    source: "dynamic"
-});
