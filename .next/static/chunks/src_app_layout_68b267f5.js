1c13864e3ec506e7229e63fe9068cdd5fbeb67ab
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-08-06 13:48:44 +0600

    branch created

---
+++ .next/static/chunks/src_app_layout_68b267f5.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__a2f1cdd3._.css",
+  "static/chunks/_9e03e1a4._.js"
+],
+    source: "dynamic"
+});
